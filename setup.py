af8a861627eb542da6501f0b3ff23bb635501f99
joshcoales <joshua@coales.co.uk>
2019-12-07 19:44:20 +0000

    Updating setup.py list of python versions to match those tested on travis

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         'Operating System :: OS Independent',
         "Programming Language :: Python",
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
